c2499329e837c0af5c2253b8661065e659b29586
nazneen jackaria <n_jackaria@hotmail.com>
2023-03-29 21:28:21 +0400

    rails new

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,29 @@
+class GamesController < ApplicationController
+  def new
+    @letters = ('A'..'Z').to_a.sample(10)
+  end
+
+  def score
+  end
+end
+begin
+
+rescue => exception
+
+ensure
+
+end
+
+rescue => exception
+
+ensure
+
+end
+<er>
+begin
+
+rescue => exception
+
+ensure
+
+end
